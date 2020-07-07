527a21e8e46422c638672eedcf17d886d080723d
Anush222 <Anush222@gmail.com>
2020-07-07 22:14:18 +0545

    login validation

--- driverapp/app/src/main/java/com/example/driverapp/LoginActivity.kt
+++ driverapp/app/src/main/java/com/example/driverapp/LoginActivity.kt
@@ -1,4 +1,54 @@
 package com.example.driverapp
 
-class LoginActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+
+        btn_sign_up.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
+
+        btn_log_in.setOnClickListener {
+            doLogin()
+        }
+
+
+    }
+
+    private fun doLogin() {
+        if (tv_username.text.toString().isEmpty()) {
+            tv_username.error = "Please enter email"
+            tv_username.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(tv_username.text.toString()).matches()) {
+            tv_username.error = "Please enter valid email"
+            tv_username.requestFocus()
+            return
+        }
+
+        if (tv_password.text.toString().isEmpty()) {
+            tv_password.error = "Please enter password"
+            tv_password.requestFocus()
+            return
+        }
+    }
 }
