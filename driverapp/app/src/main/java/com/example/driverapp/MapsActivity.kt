a3676be473b6fd1be99cb8c0e9f84d35b80598c1
Pukar Subedi <pukars57@gmail.com>
2020-07-08 20:55:58 +0545

    adding packages and modifying onCreate method

--- driverapp/app/src/main/java/com/example/driverapp/MapsActivity.kt
+++ driverapp/app/src/main/java/com/example/driverapp/MapsActivity.kt
@@ -1,43 +1,44 @@
 package com.example.driverapp
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-import com.google.android.gms.maps.CameraUpdateFactory
+import androidx.appcompat.app.AppCompatActivity
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import androidx.core.app.ActivityCompat
+import androidx.appcompat.widget.SwitchCompat
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Marker
+import com.example.driverapp.helper.*
+//import com.example.driverapp.interfaces.IPositiveNegativeListener
+//import com.example.driverapp.interfaces.LatLngInterpolator
+//import com.example.driverapp.model.Driver
+import kotlinx.android.synthetic.main.activity_login.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity() {
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+                .findFragmentById(R.id.supportMap) as SupportMapFragment
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
 }
